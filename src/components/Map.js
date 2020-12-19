fd8df20e4126924c3c9870e7a0979b141d5727c5
PrzemekCraker <przemekzbigniew@gmail.com>
2020-12-19 18:16:23 +0100

    configuring-map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,40 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
-  return <Text>Map</Text>;
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 5 + i * 0.001,
+        longitude: 5 + i * 0.001,
+        latitudeDelta: 5 + i * 0.001,
+        longitudeDelta: 5 + i * 0.001,
+      });
+    } else {
+    }
+  }
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 5,
+        longitude: 5,
+        latitudeDelta: 5,
+        longitudeDelta: 5,
+      }}
+    >
+      {" "}
+      <Polyline coordinates={points} />{" "}
+    </MapView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,40 @@
-import React from "react";
+import '../_mockLocation' //testing 
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import { Text } from "react-native-elements";
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => 
+      console.log(location))
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {err ? <Text>Please enable to track location</Text>}
     </SafeAreaView>
   );
 };
