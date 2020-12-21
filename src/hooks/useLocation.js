7b27bb5eb801904507e0ca3d7867c68bdb2a3766
PrzemekCraker <przemekzbigniew@gmail.com>
2020-12-21 15:09:57 +0100

    disable-tracking-out-create-track-screen

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -5,13 +5,17 @@ import {
   watchPositionAsync,
 } from "expo-location";
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
 
   const startWatching = async () => {
     try {
       const { granted } = await requestPermissionsAsync();
-      await watchPositionAsync(
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+      const sub = await watchPositionAsync(
         {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
@@ -19,16 +23,20 @@ export default (callback) => {
         },
         callback
       );
+      setSubscriber(sub);
     } catch (e) {
       setErr(e);
     }
   };
-  try {
-  } catch (error) {}
 
   useEffect(() => {
-    startWatching();
-  }, []);
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      subscriber.remove();
+      setSubscriber(null);
+    }
+  }, [shouldTrack]);
 
   return [err];
 };
